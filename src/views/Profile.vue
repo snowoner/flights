b42477a3beb67d5a6794cbf8cbc3d5f00a2913b3
oscar urgelles <oscar.urgelles@gmail.com>
2019-05-21 12:25:39 +0200

    perfile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -340,13 +340,50 @@ export default {
           var uploadImage = profileImg.put(this.imageFile);
           var usageImage = storage.ref(profileImg.fullPath);
 
+  uploadImage.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+  function(snapshot) {
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+
+  // A full list of error codes is available at
+  // https://firebase.google.com/docs/storage/web/handle-errors
+  switch (error.code) {
+    case 'storage/unauthorized':
+      // User doesn't have permission to access the object
+      break;
+
+    case 'storage/canceled':
+      // User canceled the upload
+      break;
+
+ 
+
+    case 'storage/unknown':
+      // Unknown error occurred, inspect error.serverResponse
+      break;
+  }
+}, ()=>{
+   uploadImage.snapshot.ref.getDownloadURL().then(downloadURL=> {
+    console.log('File available at', downloadURL);
+ 
           usageImage
             .getDownloadURL()
             .then(url => {
               this.user.user.updateProfile({
-                  photoURL: url
+                  photoURL: downloadURL
                 })
                 .then(() => {
+                  this.upImg();
                   this.$forceUpdate();
                   
                 })
@@ -357,7 +394,12 @@ export default {
             .catch(error => {
               console.log(error);
             });
-            this.upImg();
+            
+       });
+}
+);
+
+
     },
     onFilePicked(e) {
       const files = e.target.files;
